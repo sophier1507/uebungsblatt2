1f36e01b8212304b5a552d8a7ccab3a92a03e011
sophier1507 <sophieroesgen@gmail.com>
2025-10-27 13:13:36 +0100

    Hsg1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,13 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		int z = 17;
+		System.out.println("Vorheriger Wert von z: " + z);
+		if (z > 10) {
+			z -= 10;
+		}
+		System.out.println("Neuer Wert von z: " + z);
+	}
+}

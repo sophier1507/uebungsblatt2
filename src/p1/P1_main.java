c9d037b4446283ee61b240af6bc52f9ec7fb3d33
sophier1507 <sophieroesgen@gmail.com>
2025-10-27 12:56:52 +0100

    Hausaufgabe2_2

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -3,7 +3,7 @@ package p1;
 import java.util.Scanner;
 
 public class P1_main {
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 	Scanner s = new Scanner(System.in);
 	String zeile = new String();
 	zeile = s.nextLine();
---
+++ src/p2/P2_main.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+	int i = 2;
+	boolean b = true;
+	if (i > 10) {
+		b = true;
+	}
+	System.out.println(b);
+	}
+
+}
